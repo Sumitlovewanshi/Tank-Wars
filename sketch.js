9ce3e45804272ec8c96781c0037db08a55f9a5e5
Sumitlovewanshi <71924587+Sumitlovewanshi@users.noreply.github.com>
2020-12-21 15:26:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var tank ,brick,stone,enemy_tank;
+function preload(){
+  
+  tank = loadImage("Images/Tank.png");
+  brick = loadImage("Images/Brick.png");
+  stone = loadImage("Images/Brick2.png");
+  
+}
+
+function setup(){
+  tank = createSprite(10,10)
+}
+
+
+function draw(){
+  var canvas = createCanvas(800,800);
+}
---
+++ tank.js
@@ -0,0 +1,13 @@
+class Tank {
+    constructor(x,y,width,height) {
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
